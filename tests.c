128be381a4d22300457cdf4cbd06f4a602e058d0
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2022-03-23 19:01:24 +0300

    Tests corrected due to the latest commit changes

--- tests.c
+++ tests.c
@@ -1,94 +1,92 @@
 #include "graph.h"
 #include <stdio.h>
 
-int main()
-{
+int main() {
     graph *graph = NULL;
     GRAPH_ERR err;
 
-    graph = Init_Graph(-1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_1\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_1\t->\tPASSED\n");
+    {
 
-    graph = Init_Graph(5, &err);
-    if (graph == NULL)
-        fprintf(stdout, "Test_2\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_2\t->\tPASSED\n");
+        graph = Init_Graph(&err);
+        if (err == EMALLOC)
+            fprintf(stdout, "Test_1\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_1\t->\tPASSED\n");
 
-    Push_Edge(NULL, 1, 1, 1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_3.1\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_3.1\t->\tPASSED\n");
+    }
 
-    Push_Edge(graph, 1, 2, 2, &err);
-    if (err == EINVARG)
-        fprintf(stdout, "Test_3.2\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_3.2\t->\tPASSED\n");
+    {
+        Push_Edge(NULL, 1, 1, 1, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_2.1\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_2.1\t->\tPASSED\n");
 
-    Push_Edge(graph, -1, 1, 1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_3.3\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_3.3\t->\tPASSED\n");
+        Push_Edge(graph, -1, 1, 2, &err);
+        if (err == EINVARG)
+            fprintf(stdout, "Test_2.2\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_2.2\t->\tPASSED\n");
 
-    Push_Edge(graph, 1, -1, 1, &err);
-    if (err == EINVARG)
-        fprintf(stdout, "Test_3.4\t->\tPASSED\n");
-    else
-        fprintf(stdout, "Test_3.4\t->\tFAILED\n");
+        Push_Edge(graph, 1, 1, 2, &err);
+        if (err == EINVARG)
+            fprintf(stdout, "Test_2.3\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_2.3\t->\tPASSED\n");
 
-    Push_Edge(graph, 1, 1, -1, &err);
-    if (err == EINVARG)
-        fprintf(stdout, "Test_3.5\t->\tPASSED\n");
-    else
-        fprintf(stdout, "Test_3.5\t->\tFAILED\n");
+        Push_Edge(graph, 1, 3, 1, &err);
+        if (err == EINVARG)
+            fprintf(stdout, "Test_2.4\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_2.4\t->\tPASSED\n");
 
-    Push_Edge(graph, 1, 10, 1, &err);
-    if (err == EINVARG)
-        fprintf(stdout, "Test_3.6\t->\tPASSED\n");
-    else
-        fprintf(stdout, "Test_3.6\t->\tFAILED\n");
+    }
 
-    Pop_Edge(NULL, 1, 1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_4.1\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.1\t->\tPASSED\n");
 
-    Pop_Edge(graph, 2, 2, &err);
-    if (err == EINVARG)
-        fprintf(stdout, "Test_4.2\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.2\t->\tPASSED\n");
+    {
 
-    Pop_Edge(graph, -1, 1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_4.3\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.3\t->\tPASSED\n");
+        Pop_Edge(NULL, 1, 1, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_3.1\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.1\t->\tPASSED\n");
 
-    Pop_Edge(graph, 1, -1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_4.4\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.4\t->\tPASSED\n");
+        Pop_Edge(graph, 2, 2, &err);
+        if (err == EINVARG)
+            fprintf(stdout, "Test_3.2\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.2\t->\tPASSED\n");
 
-    Pop_Edge(graph, 10, 1, &err);
-    if (err != EINVARG)
-        fprintf(stdout, "Test_4.5\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.5\t->\tPASSED\n");
+        Pop_Edge(graph, -1, 1, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_3.3\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.3\t->\tPASSED\n");
 
-    int temp = Pop_Edge(graph, 2, 2, &err);
-    if (temp != 1)
-        fprintf(stdout, "Test_4.6\t->\tFAILED\n");
-    else
-        fprintf(stdout, "Test_4.6\t->\tPASSED\n");
+        Pop_Edge(graph, 1, -1, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_3.4\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.4\t->\tPASSED\n");
 
+        Pop_Edge(graph, 10, 1, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_3.5\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.5\t->\tPASSED\n");
+
+        Pop_Edge(graph, 1, 10, &err);
+        if (err != EINVARG)
+            fprintf(stdout, "Test_3.6\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.6\t->\tPASSED\n");
+
+        int temp = Pop_Edge(graph, 1, 2, &err);
+        if (temp != 1)
+            fprintf(stdout, "Test_3.7\t->\tFAILED\n");
+        else
+            fprintf(stdout, "Test_3.7\t->\tPASSED\n");
+    }
     return 0;
 }
+
